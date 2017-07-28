b6fa4dea77760641ec783f2496855b311b95ed7a
Steven Ventura <8stevenventura@gmail.com>
2017-07-28 15:23:43 -0500

    iniital commit
    please

---
+++ juliancode.cpp
@@ -0,0 +1,72 @@
+#include "SFML/Graphics.hpp"
+#include <iostream>
+#include <string>
+//#include "ResourcePath.hpp"
+
+
+int main()
+{
+    std::string resourcePath = "C:\\Users\\Yoloswag\\Desktop\\";
+	sf::RenderWindow window(sf::VideoMode(600, 600), "Surface");
+
+	window.setFramerateLimit(60);
+
+	sf::Sprite sprite;
+
+	sf::Texture texture;
+
+	if (!texture.loadFromFile(resourcePath + "gnomechild.png"))
+	{
+    	std::cout << "reeeeeee file path " << std::endl;
+	}
+
+	sprite.setTexture(texture);
+
+	sf::Clock clock;
+
+
+	while (window.isOpen())
+	{
+    	sf::Event event;
+
+    	while (window.pollEvent(event))
+    	{
+        	if (event.type == sf::Event::Closed)
+            	window.close();
+    	}
+
+    	sf::Time time = clock.getElapsedTime();
+
+    	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    	{
+        	sprite.move(sf::Vector2f(-0.5 * time.asMilliseconds(), 0));
+    	}
+
+    	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    	{
+        	sprite.move(sf::Vector2f(0, -0.5 * time.asMilliseconds()));
+    	}
+
+    	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    	{
+        	sprite.move(sf::Vector2f(0, 0.5 * time.asMilliseconds()));
+    	}
+
+    	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    	{
+        	sprite.move(sf::Vector2f(0.5 * time.asMilliseconds(), 0));
+    	}
+
+    	clock.restart().asMilliseconds();
+
+    	window.clear();
+
+    	window.draw(sprite);
+
+    	window.display();
+
+    	std::cout << 1.0f / time.asSeconds() << std::endl; //Prints the frame rate to the console.
+	}
+}
+
+
